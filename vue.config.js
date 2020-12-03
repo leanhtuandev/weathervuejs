a598853670641bd58086b6a03dce642ffd8f6990
leanhtuandev <nifuuji@gmail.com>
2020-12-03 10:16:17 +0700

    deploy

--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? 'https://github.com/leanhtuandev/weathervuejs.git' // Thay tên repository của các bạn vào đây nhé
-      : '/'
-  }
